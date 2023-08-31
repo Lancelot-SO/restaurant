d0b4ac6b869d657e5e274483a57b8126a68f94da
Lancelot-SO <fsowah001@gmail.com>
2023-08-31 23:34:28 +0000

    create db with prisma,postgresql and docker

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -4,3 +4,7 @@ import { NextResponse } from "next/server"
 export const GET = () => {
     return new NextResponse( "Hello", { status: 200});
 }
+
+export const POST = () => {
+    return new NextResponse( "Hello", { status: 200});
+}
